5d6c7d52c632bbaab4d2b788cdd42940bc5e4f5b
arpitpokharel <arpitpokharel2@gmail.com>
2021-03-23 10:06:15 +0545

    changes in Read me

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # seleniumtestcase
 - Open selenium test case.py
+- Install selenium  
 - Install correct driver version for your chrome from https://chromedriver.chromium.org/
 - Add path of the driver on the code 
 - Run 
--- venv/Selenium test case.py
+++ venv/Selenium test case.py
@@ -3,14 +3,16 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 path = "C:\chrome driver\chromedriver.exe"
 driver = webdriver.Chrome(path)
 driver.get("http://test.fcubecinemas.com/")
 driver.maximize_window()
 
+
 time.sleep(5)
-Time_select = driver.find_element_by_xpath("//span[contains(text(),'23')]")
+Time_select = driver.find_element_by_xpath("//span[contains(text(),'25')]")
 Time_select.click()
 time.sleep(4)
 
@@ -19,61 +21,77 @@ print(len(links))
 for link in links:
     print(link.text)
 
+
+time.sleep(10)
 Now_showing = driver.find_element_by_link_text("Vijay The Master (PG)")
 Now_showing.click()
-time.sleep(7)
+time.sleep(10)
 driver.find_element_by_link_text("04:00 PM").click()
 
 #Login
 
-time.sleep(3)
+time.sleep(10)
 Email = driver.find_element_by_xpath("//input[@id='UserName']").send_keys("motung")
-time.sleep(2)
+
+time.sleep(3)
 Password = driver.find_element_by_xpath("//input[@id='Password']").send_keys("Nirvana3")
-time.sleep(2)
+
+time.sleep(3)
 Go = driver.find_element_by_xpath("//button[contains(text(),'GO')]").click()
-time.sleep(4)
+time.sleep(10)
 
 #Selecting tickets
+
 Ticket1 = driver.find_element_by_xpath("//button[@id='393']")
 Ticket1.click()
-time.sleep(3)
+
+time.sleep(5)
 Ticket2 = driver.find_element_by_xpath("//button[@id='390']")
 Ticket2.click()
-time.sleep(3)
+
+time.sleep(5)
 Ticket3 = driver.find_element_by_xpath("//button[@id='389']")
 Ticket3.click()
-time.sleep(3)
+
+time.sleep(5)
 Ticket4 = driver.find_element_by_xpath("//button[@id='388']")
 Ticket4.click()
-time.sleep(3)
+
+time.sleep(5)
 Ticket5 = driver.find_element_by_xpath("//button[@id='358']")
 Ticket5.click()
-time.sleep(3)
+time.sleep(5)
 #diselecting a seat
 driver.find_element_by_xpath("//button[@id='358']").click()
 
 #Reserving tickets
-time.sleep(3)
+
+time.sleep(5)
 Reserve = driver.find_element_by_xpath("//button[@id='btn-reserve']")
 Reserve.click()
-time.sleep(4)
+
+time.sleep(10)
 driver.find_element_by_xpath("//button[@id='btn-continue-reserve']").click()
-time.sleep(7)
+
+time.sleep(10)
 driver.find_element_by_xpath("//body/div[@id='confirmationMessage']/div[3]/button[1]").click()
 
 #My account page
 time.sleep(5)
 Collaps_btn = driver.find_element_by_tag_name("button")
 Collaps_btn.click()
-time.sleep(3)
+
+time.sleep(5)
 Cancel_btn = driver.find_element_by_xpath("//button[contains(text(),'Cancel Selected')]")
 Cancel_btn.click()
+
 time.sleep(5)
 Yes = driver.find_element_by_xpath("//div[contains(text(),'Yes')]")
 Yes.click()
+
 time.sleep(5)
 Popup_cose = driver.find_element_by_xpath("//div[@id='popupMessage']//button[@id='btn-close']")
 Popup_cose.click()
+
 time.sleep(5)
 driver.close()
